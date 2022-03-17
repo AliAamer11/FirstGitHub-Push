77397795a3b8e8486a50ead6f45923e258e7b89e
AliAamer <DevAliAamer@gmail.com>
2022-03-17 22:05:46 +0200

    modified hello to hello first change

--- FirstGitHub-Push/Startup.cs
+++ FirstGitHub-Push/Startup.cs
@@ -32,7 +32,7 @@ namespace FirstGitHub_Push
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello first change!");
                 });
             });
         }
